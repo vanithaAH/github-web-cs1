083e7db26805da7ae8fcc27586b2f672091299ef
vanithaAH <94234261+vanithaAH@users.noreply.github.com>
2022-07-20 12:25:15 +0530

    Create HelloCode.cs

---
+++ HelloCode.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSharpSolutions
+{
+    class HelloCode
+    {
+         static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
